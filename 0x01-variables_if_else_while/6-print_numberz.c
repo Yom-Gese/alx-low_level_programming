16b207bf4f7bf95c42ea03af851a40c43c7ac936
Yomy Gesese <yomygesese@gmail.com>
2022-06-03 11:48:02 -0700

    all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 /**
- * main - Prints numbers of base ten.
+ * main - prints numbers 0-9 without using char or printf/puts,
+ * and putchar 2x max
 (*
  * Return: 0 on success
  */
 int main(void)
 {
-	printf("0123456789\n");
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
