05b743303ab866c2a4aab814e587083333e4bbca
Yomy Gesese <yomygesese@gmail.com>
2022-07-01 02:20:16 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+/**
+ * Auth: Yom-Gese 
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-more_malloc_free directory.
+ */
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
