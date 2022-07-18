19172704f31e5c61771ada32ec59894e83428dba
Yomy Gesese <yomygesese@gmail.com>
2022-07-18 08:16:57 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,46 +1,37 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - deletes the node at index of a listint_t list.
- * @head: pointer to the list.
- * @index: position of the node to delete.
- * Return: 1 if it succeeded, -1 if it failed.
- **/
+ * delete_nodeint_at_index - deletes the node at index index
+ * of a listint_t linked list.
+ * @head: double pointer to the first node in the list
+ * @index: index of the node to delete
+ *
+ * Return: pointer to the indexed node
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *aux_node = *head;
-	listint_t *node_to_delete = *head;
-	unsigned int idx;
-	unsigned int cont = 0;
+	unsigned int i;
+	listint_t *current, *next;
 
-	/* border case for empty list */
-	if (!(*head))
+	if (head == NULL || *head == NULL)
 		return (-1);
-
-	/* border case for delete at the beginning */
 	if (index == 0)
 	{
-		*head = node_to_delete->next;
-		free(node_to_delete);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 		return (1);
 	}
-
-	/* search of position to delete */
-	idx = index - 1;
-	while (aux_node && cont != idx)
+	current = *head;
+	for (i = 0; i < index - 1; i++)
 	{
-		cont++;
-		aux_node = aux_node->next;
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
 	}
-
-	/* general case */
-	if (cont == idx && aux_node)
-	{
-		node_to_delete = aux_node->next;
-		aux_node->next = node_to_delete->next;
-		free(node_to_delete);
-		return (1);
-	}
-
-	return (-1);
+	next = current->next;
+	current->next = next->next;
+	free(next);
+	return (1);
 }
