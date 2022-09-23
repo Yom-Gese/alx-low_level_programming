7d81e9310dba94ef4635492a5d42dfeecff29397
Yomy Gesese <yomygesese@gmail.com>
2022-09-23 04:34:35 -0700

    hash table

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,17 +1,18 @@
-#include "hash_tables.h"
-
 /**
- *  * hash_djb2 - Hash function implementing the djb2 algorithm.
- *   * @str: The string to hash.
- *    *
- *     * Return: The calculated hash.
-*/
+ * hash_djb2 - Hash function, takes a string and turns it into a hash number
+ * @str: the string, a constant value unrepeated in the hash table
+ *
+ * Return: The hash number
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-           unsigned long int hash;
-	   int c;
-	   hash = 5381;
-	   while ((c = *str++))
-		   hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	   return (hash);
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
